52491a67ca079765b50be142ddd61d1de4e5062a
Angelica <Angelica137@users.noreply.github.com>
2020-10-06 22:23:35 +0100

    add content to card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import Owner from "../Owner/owner";
 
 const CardWrapper = styled.div`
   text-align: left;
@@ -16,9 +17,27 @@ const Title = styled.h2`
   text-color: #303545;
   border-bottom: 1px solid #303545;
 `;
+
+const Meta = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 2%;
+`;
+
+const Count = styled.div`
+  flex-basis: 80%;
+`;
+
 const Card = () => (
   <CardWrapper>
     <Title>this is a card</Title>
+    <Meta>
+      <Count>
+        {`Views: ${data.view_count} | Answers: ${data.answer_count}`}
+      </Count>
+      <Owner data={data.owner} />
+    </Meta>
   </CardWrapper>
 );
 
