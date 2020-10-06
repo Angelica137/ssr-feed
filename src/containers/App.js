1e15c1cbc3435de4f4c54ef97904e76f46d4b82f
Angelica <Angelica137@users.noreply.github.com>
2020-10-06 20:52:48 +0100

    import component and create app component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import styled, { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
@@ -15,27 +15,31 @@ const AppWrapper = styled.div`
   text-align: center;
 `;
 
-function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </>
-  );
+class App extends Component {
+  render() {
+    return (
+      <>
+        <GlobalStyle />
+        <AppWrapper>
+          <div className="App">
+            <header className="App-header">
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        </AppWrapper>
+      </>
+    );
+  }
 }
 
 export default App;
