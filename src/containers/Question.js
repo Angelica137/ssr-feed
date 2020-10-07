85552f4402c895fd1cff00b36cb9d16bc6a40e5d
Angelica <Angelica137@users.noreply.github.com>
2020-10-07 21:47:36 +0100

    create quesitons styling

---
+++ src/containers/Question.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import styles from "styled-components";
+
+const QuestionWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  margin: 5%;
+`;
