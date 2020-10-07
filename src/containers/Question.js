bc7168c4bb47aa7a7fd8e326dadba13bc6fa9e1f
Angelica <Angelica137@users.noreply.github.com>
2020-10-07 21:51:50 +0100

    create question component

--- src/containers/Question.js
+++ src/containers/Question.js
@@ -7,3 +7,29 @@ const QuestionWrapper = styled.div`
   flex-direction: column;
   margin: 5%;
 `;
+
+const Alert = styled.div`
+  text-align: center;
+`;
+
+class Question extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+      loading: true,
+      error: "",
+    };
+  }
+
+  render() {
+    const { data, loading, error } = this.state;
+
+    if (loading || error) {
+      return <Alert>{loading ? "Loading..." : error}</Alert>;
+    }
+    return <QuestionWrapper></QuestionWrapper>;
+  }
+}
+
+export default Question;
