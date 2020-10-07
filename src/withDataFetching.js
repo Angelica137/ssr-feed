7a3cf6e8b55858a2abef4d07975fcb8198da0cb2
Angelica <Angelica137@users.noreply.github.com>
2020-10-07 21:19:51 +0100

    add with data fetching

---
+++ src/withDataFetching.js
@@ -0,0 +1,39 @@
+import React from "ract";
+
+export default function withDataFetching(WrappedComponent) {
+  class WithDataFetching extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        loading: props.loadingMessage,
+      };
+    }
+
+    async componentDidMount() {
+      try {
+        const data = await fetch(this.props.dataSource);
+        const dataJSON = await data.json();
+
+        if (dataJSON) {
+          this.setState({
+            data: dataJSON,
+            loading: false,
+          });
+        }
+      } catch (error) {
+        this.setState({
+          loading: error.messeage,
+        });
+      }
+    }
+
+    render() {
+      const { data, loading } = this.state;
+
+      return <WrappedComponent data={data} loading={loading} {...this.props} />;
+    }
+  }
+
+  WithDataFetching.displayName = `WithDataFetching(${WrappedComponent.name})`;
+  return WithDataFetching;
+}
