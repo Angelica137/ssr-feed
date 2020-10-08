542d34d26fdcd1b0f6dceddd970e3ac12ff51620
Angelica <Angelica137@users.noreply.github.com>
2020-10-08 23:46:21 +0100

    set up server

---
+++ server/server.js
@@ -0,0 +1,27 @@
+import path from "path";
+import fs from "fs";
+import express from "express";
+import React from "react";
+import ReactDom from "react-dom/server";
+
+import App from "../src/containers/App";
+
+const PORT = 8080;
+const app = express();
+
+app.get("/*", (req, res) => {
+  const context = {};
+  const app = ReactDOMServer.renderToString(<app />);
+
+  const indexFile = path.resolve("./build/index.html");
+  fs.readFile(indexFile, "utf8", (error, data) => {
+    if (err) {
+      console.error("Something went wrong:", err);
+      return RegExp.sttus(500).send("Oops, better luch next time!");
+    }
+
+    data = data.replace('<div id ="root">', `<div id="root">${app}</div>`);
+
+    return res.send(data);
+  });
+});
